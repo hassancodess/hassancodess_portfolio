b5c3e15b43dae28c7e04326db1d084c909faff84
hassancodess <dev.hassancodess@gmail.com>
2024-12-21 13:15:19 +0500

    feat: update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,67 +1,69 @@
-import { cn } from "./ui/utils.ts";
-import { Lucide } from "./icons";
+import { cn } from './ui/utils.ts'
+import { Lucide } from './icons'
 
 // TODO : update projects
 const projects = [
   {
-    title: "Home Decor App",
-    description: "Expo, React Native, Tamagui, Supabase",
-    icon: Lucide.IconGithub,
-    url: "https://github.com/hassancodess/home-decor-app",
+    title: 'eddii',
+    description: 'React Native, Tamagui, AWS Amplify, Maestro, Bitrise',
+    icon: Lucide.IconSmartphone,
+    url: 'https://apps.apple.com/us/app/eddii-complete-diabetes-care/id6450377749',
   },
   {
-    title: "Ecommerce API",
-    description: "Express.js, PrismaORM, Vitest, Docker",
+    title: 'OurMaids Inc.',
+    description: 'Expo, Next.js, Express.js, Docker, DigitalOcean',
     icon: Lucide.IconGithub,
-    url: "https://github.com/hassancodess/ecommerce-api",
+    url: 'https://apps.apple.com/us/app/our-maids-inc/id1626136656',
   },
   {
-    title: "Jobspot App",
-    description: "Expo, React Native, Tamagui, Firebase",
+    title: 'Jobspot App',
+    description: 'Expo, React Native, Tamagui, Firebase',
     icon: Lucide.IconGithub,
-    url: "https://github.com/hassancodess/jobspot-app",
+    url: 'https://github.com/hassancodess/jobspot-app',
   },
-];
+]
 
 export default () => {
   return (
-    <div className="flex items-center justify-center p-4 md:p-5 xl:p-6 w-full h-full">
-      <ul className="grid grid-cols-1 gap-3 w-full h-full">
+    <div className='flex items-center justify-center p-4 md:p-5 xl:p-6 w-full h-full'>
+      <ul className='grid grid-cols-1 gap-3 w-full h-full'>
         {projects.map((project) => {
           return (
-            <li key={project.title} className="flex items-center">
+            <li key={project.title} className='flex items-center'>
               <a
                 href={project.url}
                 className={cn(
-                  "group",
-                  "flex items-center justify-between px-4 py-3 w-full",
-                  "bg-neutral-2 hover:bg-neutral-3",
-                  "outline-none border border-neutral-4 hover:border-neutral-6 rounded-3xl",
-                  "focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-7 focus-visible:ring-offset-neutral-1"
+                  'group',
+                  'flex items-center justify-between px-4 py-3 w-full',
+                  'bg-neutral-2 hover:bg-neutral-3',
+                  'outline-none border border-neutral-4 hover:border-neutral-6 rounded-3xl',
+                  'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-neutral-7 focus-visible:ring-offset-neutral-1'
                 )}
-                target="_blank"
-                aria-label="Open"
+                target='_blank'
+                aria-label='Open'
               >
-                <div className="flex flex-row items-stretch gap-5 pl-2">
-                  <div className="flex items-center justify-center">
+                <div className='flex flex-row items-stretch gap-5 pl-2'>
+                  <div className='flex items-center justify-center'>
                     <project.icon
                       size={24}
-                      className="transition duration-300 group-hover:-translate-y-1 text-neutral-10 group-hover:text-neutral-11"
+                      className='transition duration-300 group-hover:-translate-y-1 text-neutral-10 group-hover:text-neutral-11'
                     />
                   </div>
 
                   <div>
-                    <h3 className="transition duration-300 group-hover:translate-x-1 text-neutral-12 text-sm">{project.title}</h3>
-                    <div className="transition duration-300 group-hover:translate-x-px text-neutral-10 group-hover:text-neutral-11 text-xs">
+                    <h3 className='transition duration-300 group-hover:translate-x-1 text-neutral-12 text-sm'>
+                      {project.title}
+                    </h3>
+                    <div className='transition duration-300 group-hover:translate-x-px text-neutral-10 group-hover:text-neutral-11 text-xs'>
                       {project.description}
                     </div>
                   </div>
                 </div>
               </a>
             </li>
-          );
+          )
         })}
       </ul>
     </div>
-  );
-};
+  )
+}
--- src/components/icons/lucide.tsx
+++ src/components/icons/lucide.tsx
@@ -1,33 +1,37 @@
-import { forwardRef } from "react";
-import type { ReactNode, SVGProps } from "react";
+import { forwardRef } from 'react'
+import type { ReactNode, SVGProps } from 'react'
 
-type BaseProps = Omit<SVGProps<SVGSVGElement>, "role" | "viewBox" | "xmlns"> & { size?: number };
+type BaseProps = Omit<SVGProps<SVGSVGElement>, 'role' | 'viewBox' | 'xmlns'> & {
+  size?: number
+}
 
-const Base = forwardRef<SVGSVGElement, BaseProps>(({ d, size = 16, width = size, height = size, ...props }, ref) => (
-  <svg
-    ref={ref}
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-    width={width}
-    height={height}
-    role="img"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  />
-));
+const Base = forwardRef<SVGSVGElement, BaseProps>(
+  ({ d, size = 16, width = size, height = size, ...props }, ref) => (
+    <svg
+      ref={ref}
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      {...props}
+      width={width}
+      height={height}
+      role='img'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+    />
+  )
+)
 
-type IconProps = Omit<BaseProps, "children">;
+type IconProps = Omit<BaseProps, 'children'>
 
 const create = (children: ReactNode) =>
   forwardRef<SVGSVGElement, IconProps>((props, ref) => (
     <Base ref={ref} {...props}>
       {children}
     </Base>
-  ));
+  ))
 
 /**
  * How to add icons ?
@@ -53,34 +57,37 @@ const create = (children: ReactNode) =>
 
 export const IconArrowRight = create(
   <>
-    <path d="M5 12h14" />
-    <path d="m12 5 7 7-7 7" />
+    <path d='M5 12h14' />
+    <path d='m12 5 7 7-7 7' />
   </>
-);
+)
 
 export const IconExternalLink = create(
   <>
-    <path d="M15 3h6v6" />
-    <path d="M10 14 21 3" />
-    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+    <path d='M15 3h6v6' />
+    <path d='M10 14 21 3' />
+    <path d='M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6' />
   </>
-);
+)
 
 export const IconImage = create(
   <>
-    <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-    <circle cx="9" cy="9" r="2" />
-    <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21" />
+    <rect width='18' height='18' x='3' y='3' rx='2' ry='2' />
+    <circle cx='9' cy='9' r='2' />
+    <path d='m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21' />
   </>
-);
+)
 
 export const IconGithub = create(
   <>
-  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
-  <path d="M9 18c-4.51 2-5-2-7-2"/>
+    <path d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4' />
+    <path d='M9 18c-4.51 2-5-2-7-2' />
   </>
-);
+)
 
-
-
-
+export const IconSmartphone = create(
+  <>
+    <rect width='14' height='20' x='5' y='2' rx='2' ry='2' />
+    <path d='M12 18h.01' />
+  </>
+)
